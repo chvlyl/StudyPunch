26139fc93a85f6687c5432b05cf50ea4b3d73c04
Jinwu76 <jinwu76@Jins-MacBook-Pro.local>
2025-07-20 12:18:19 -0700

    Fix auth7

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Sidebar from './Sidebar'
-import AuthGuard from './AuthGuard'
 
 interface AppLayoutProps {
   children: React.ReactNode
@@ -9,18 +8,16 @@ interface AppLayoutProps {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <AuthGuard>
-      <div className="flex h-screen bg-gray-100">
-        {/* Sidebar */}
-        <Sidebar />
-        
-        {/* Main Content */}
-        <main className="flex-1 overflow-y-auto">
-          <div className="p-6">
-            {children}
-          </div>
-        </main>
-      </div>
-    </AuthGuard>
+    <div className="flex h-screen bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content */}
+      <main className="flex-1 overflow-y-auto">
+        <div className="p-6">
+          {children}
+        </div>
+      </main>
+    </div>
   )
 } 
